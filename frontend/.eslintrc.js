a00f5d7e03ecb44a7fc06153c977a8af4fdaecdf
Svante Bengtson <svante.b37@gmail.com>
2020-10-02 00:33:58 +0200

    base vite frontend

---
+++ frontend/.eslintrc.js
@@ -0,0 +1,13 @@
+module.exports = {
+  root: true,
+  env: {
+    node: true,
+  },
+  extends: ['plugin:vue/vue3-essential', '@vue/standard', '@vue/typescript/recommended'],
+  parserOptions: {
+    ecmaVersion: 2020,
+  },
+  rules: {
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+  }
+}
